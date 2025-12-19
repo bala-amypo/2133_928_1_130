65c23eaa414b9df1396269ac0703e95ffffa69fa
bala-amypo <bala@amypo.in>
2025-12-19 09:16:21 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/FraudAlertRecord.java
@@ -0,0 +1,49 @@
+// FraudAlertRecord.java
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "fraud_alert_records")
+public class FraudAlertRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String deviceId;
+    private String alertLevel;
+    private String message;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getAlertLevel() {
+        return alertLevel;
+    }
+
+    public void setAlertLevel(String alertLevel) {
+        this.alertLevel = alertLevel;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
