2e2ea84db26410469267deddf85c44d3844d9906
bala-amypo <bala@amypo.in>
2025-12-21 15:42:58 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,27 +1,13 @@
 package com.example.demo.dto;
 
-public class AuthRequest {
-    private String username;
-    private String password;
+public class AuthResponse {
+    private String token;
 
-    // Constructors
-    public AuthRequest() {}
-    public AuthRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
+    public AuthResponse() {}
+    public AuthResponse(String token) {
+        this.token = token;
     }
 
-    // Getters and setters
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
 }
