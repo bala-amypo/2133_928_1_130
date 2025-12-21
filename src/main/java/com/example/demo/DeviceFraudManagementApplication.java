31bd61b0afdbb4b7ec46bf16c2e2bc54c1ba245a
bala-amypo <bala@amypo.in>
2025-12-21 15:51:58 +0000

    User commit

--- src/main/java/com/example/demo/DeviceFraudManagementApplication.java
+++ src/main/java/com/example/demo/DeviceFraudManagementApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeviceFraudManagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DeviceFraudManagementApplication.class, args);
     }
