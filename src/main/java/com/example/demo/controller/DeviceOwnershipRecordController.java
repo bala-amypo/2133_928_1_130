0535335b4aa1f3ed7de53ea390ea2196fefe8beb
bala-amypo <bala@amypo.in>
2025-12-25 16:48:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/DeviceOwnershipRecordController.java
+++ src/main/java/com/example/demo/controller/DeviceOwnershipRecordController.java
@@ -1,43 +1,14 @@
+
 package com.example.demo.controller;
 
-import com.example.demo.entity.DeviceOwnershipRecordEntity;
-import com.example.demo.service.DeviceOwnershipRecordService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import com.example.demo.service.impl.DeviceOwnershipServiceImpl;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/devices")
-public class DeviceOwnershipRecordController {
-
-    private final DeviceOwnershipRecordService service;
-
-    public DeviceOwnershipRecordController(DeviceOwnershipRecordService service) {
+public class DeviceOwnershipController {
+    private final DeviceOwnershipServiceImpl service;
+    
+    public DeviceOwnershipController(DeviceOwnershipServiceImpl service) {
         this.service = service;
-    }
-
-    @PostMapping
-    public ResponseEntity<DeviceOwnershipRecordEntity> register(
-            @RequestBody DeviceOwnershipRecordEntity device) {
-        return ResponseEntity.ok(service.registerDevice(device));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<DeviceOwnershipRecordEntity>> getAll() {
-        return ResponseEntity.ok(service.getAllDevices());
-    }
-
-    @GetMapping("/serial/{serialNumber}")
-    public ResponseEntity<DeviceOwnershipRecordEntity> getBySerial(
-            @PathVariable String serialNumber) {
-        return ResponseEntity.ok(service.getBySerial(serialNumber));
-    }
-
-    @PutMapping("/{id}/status")
-    public ResponseEntity<DeviceOwnershipRecordEntity> updateStatus(
-            @PathVariable Long id,
-            @RequestParam boolean active) {
-        return ResponseEntity.ok(service.updateDeviceStatus(id, active));
     }
 }
